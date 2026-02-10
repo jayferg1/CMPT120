436b1fdc5ae91287a40d07dfa448d40d6932bc28
jayferg1 <jaden.fergus2154@gmail.com>
2026-02-10 16:23:01 -0500

    debug hw assignment 3

--- Assignment 3/DebugOne.py
+++ Assignment 3/DebugOne.py
@@ -3,44 +3,41 @@ def main():
 
     #that's not right... how can we make sure it also includes equals 5?
     temp = 5
-    if temp > 5:
-        print("temp is greater than or equal to 5")
+if temp >= 5:
+    print("temp is greater than or equal to 5")
         
     
     #enter your name here
-    name = ""
+    name = "jaden"
     '''in this if/elif/else, add 4 mispellings of your name for if/elif comparisons, and then have your last elif be elif name = "your properly spelled name" '''
-    if name == "mispelling here":
-        print("That's not right!")
-    elif name == "another mispelling":
-        print("That's not right!")
+if name == "juden":
+    print("That's not right!")
+elif name == "jahden":
+    print("That's not right!")
+elif name == "jaden":
+    print("yo lowkey u got it twizzy")
+
     #etc etc, the else can be whatever you want
 
     
     
-    #we're gonna check if a user input number is even
-    #pick any number
-    even = input(int("Enter a number to find out if its even or odd"))
-    even = int(even)
-    #what do we replace the question marks with?
-    if even ? ? == ?:
-        #what would be appropriate in these print statements?
-        print("")
-    else:
-        print("")
+    even = int(input("Enter a number to find out if its even or odd"))
+if even  % 2 == 0:
+    print("even")
+else:
+    print("odd")
   
   
-    #i'm trying to do math with the numbers 2 and 4, but it's getting 3 and 5... why?
     numbers = [1,2,3,4,5]
-    print(numbers[2])
-    print(numbers[4])
-    print(numbers[4] / numbers[2])
-    print(numbers[4] * numbers[2])
-    
-    #again, why is it getting 7 and 20, I wanted 3 and 29!
-    numbers2 = [465,3,30,7,29,20,82,13,5]
-    print(numbers2[3])
-    print(numbers2[5])
+print(numbers[1])
+print(numbers[3])
+print(numbers[3] / numbers[2])
+print(numbers[3] * numbers[2])
+
+#again, why is it getting 7 and 20, I wanted 3 and 29!
+numbers2 = [465,3,30,7,29,20,82,13,5]
+print(numbers2[1])
+print(numbers2[4])
     
 main()
     
